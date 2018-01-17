a872c906ed1020e19724c80245f4cd9aaaa4b33e
Akhil Negi <AKHILNEGI007@outlook.com>
2018-01-17 17:16:02 +0530

    Adding just to see if Sonar Rules are running or not

--- src/CoderByte.API/Model/Employee.cs
+++ src/CoderByte.API/Model/Employee.cs
@@ -2,5 +2,29 @@
 {
     public class Employee
     {
+        public void Test(int param)
+        {
+            if (param == 1)
+                OpenWindow();
+            else if (param == 2)
+                CloseWindow();
+            else if (param == 1)  // Noncompliant
+                MoveWindowToTheBackground();
+        }
+
+        private void MoveWindowToTheBackground()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void CloseWindow()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OpenWindow()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
