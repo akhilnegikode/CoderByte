7dad77aca2ccd84b08f7926109eaef79b3b05edb
Akhil Negi <AKHILNEGI007@outlook.com>
2018-01-25 14:45:21 +0530

    API completed and it should be using In-Memory Database for ASP.NET Core

---
+++ src/CoderByte.API/ApiContext.cs
@@ -0,0 +1,21 @@
+﻿using CoderByte.API.Mappings;
+using CoderByte.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoderByte.API
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeMapping());
+        }
+    }
+}
---
+++ src/CoderByte.API/Controllers/EmployeesController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using CoderByte.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoderByte.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly ApiContext _context;
+
+        public EmployeesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/employees
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            var employeeList = _context.Employees;
+            return employeeList;
+        }
+
+        // GET api/employees/1
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            return employee;
+        }
+
+        // POST api/employees
+        [HttpPost]
+        public void Post([FromBody]Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+        }
+
+        // PUT api/employees/1
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Employee employee)
+        {
+            var emp = _context.Employees.Find(id);
+            emp.EmployeeName = employee.EmployeeName;
+            emp.EmployeeNumber = employee.EmployeeNumber;
+            emp.Designation = employee.Designation;
+            emp.Password = employee.Password;
+            emp.Role = employee.Role;
+            emp.ServiceLine = employee.ServiceLine;
+            _context.SaveChanges();
+        }
+
+        // DELETE api/employees/1
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null) return;
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+        }
+    }
+}
